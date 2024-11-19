59753b3d75223859067a1fff09bd12fcd1a2436a
Gizmeondevwip <59248412+Gizmeondevwip@users.noreply.github.com>
2024-11-19 21:30:13 +0530

    new end point

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors()); 
+
+app.get('/', (req, res) => {
+    res.json({ message: 'App is up and running!' });
+  });
+
 app.use('/routes/email', emailFile)
 
 module.exports = app;   
